4140c6f252ab2f807c05d690a1d59e9892871385
Saas-1 <stasnapreenko4@gmail.com>
2024-05-23 12:30:31 +0300

    Add another test

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -16,6 +16,20 @@ TEST(ValidateUsers, ValidationTrue) {
     }
 }
 
+TEST(ValidateUser2, ValidationTrue) {
+	try {
+        	Model *model = new Model();
+		model->addDatabase("test.db");
+        	User *user = new User(USER, "user1", "0b14d501a594442a01c6859541bcb3e8164d183d32937b851835442f69d5c94e");
+        	ASSERT_TRUE(model->hasUser(*user));
+        	delete model;
+        	delete user;
+    } catch (const std::exception& e) {
+    	std::cerr << "Exception caught: " << e.what() << std::endl;
+        FAIL();
+    }
+}
+
 int main(int argc, char ** argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
